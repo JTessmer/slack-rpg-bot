cab5740ffb2482afda5b318634eafdb51541a54e
Jesse Tessmer <jesse.tessmer@gmail.com>
2022-10-22 11:06:54 -0500

    Add runtime configs

---
+++ pm2.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: 'slack-rpg-bot',
+      script: 'npm',
+      args: 'start',
+      instances: 1,
+      autorestart: true,
+      max_restarts: 50,
+      watch: false,
+      env: {
+        PORT: 4000,
+        VERBOSE: true
+      }
+    }
+  ]
+}
--- src/util/env.ts
+++ src/util/env.ts
@@ -1,1 +1,2 @@
 export const PORT = process.env.PORT ? parseInt(process.env.PORT) : 80
+export const VERBOSE = process.env.VERBOSE === 'true'
