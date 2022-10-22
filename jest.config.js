84265c547965059409469e6b9baa22fb7a501d64
Jesse Tessmer <jesse.tessmer@gmail.com>
2022-10-22 11:06:54 -0500

    Improve dev support features

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest/presets/jest-with-ts',
+  preset: 'ts-jest/presets/default-esm',
   collectCoverageFrom: ['src/**/*.ts', '!src/**/index.ts', '!src/**/*.test.ts'],
   roots: ['<rootDir>/src'],
   testEnvironment: 'node',
-  moduleFileExtensions: ['ts'],
+  moduleFileExtensions: ['js', 'ts'],
   moduleNameMapper: {
     '~/(.*)': '<rootDir>/src/$1'
   },
